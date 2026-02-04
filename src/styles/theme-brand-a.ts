856f63c759a919384dbb6e97cfcaa39e1ba0d117
Kaung Myat Hein <kaungmyat9702@gmail.com>
2026-02-04 12:18:06 +0700

    fix

--- src/styles/theme-brand-a.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Do not edit directly, this file was auto-generated.
- */
-
-export const StartUpTokensBrandAButtonBackgroundDefault = "#7e22ce";
-export const StartUpTokensBrandAButtonBackgroundDisable = "#e9d5ff";
-export const StartUpTokensBrandAButtonTextDefault = "#ffffff";
